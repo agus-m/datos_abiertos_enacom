eb53e661206d36d7d355c30a6f41b1b91725d7c5
agusm <57236016+agus-m@users.noreply.github.com>
2021-04-04 09:38:42 -0300

    Add files via upload

--- Request_Enacom_Json.py
+++ Request_Enacom_Json.py
@@ -9,7 +9,7 @@ import json
 # y guarda cada archivo en una carpeta local.
 
 auth_key = "A6xbR3TrMZW6ivfxJmAzo5QMGbxLLUYjLPatSzr4"
-url = 'http://api.datosabiertos.enacom.gob.ar/api/v2/resources.json/?auth_key=' + auth_key
+url = 'http://api.datosabiertos.enacom.gob.ar/api/v2/resources.json/?auth_key={clave}'.format(clave=auth_key)
 
 headers = {"User-Agent":"Mozilla/5.0"}
 respuesta_res = requests.get(url, headers=headers).text # sin .text te da la respuesta pero no ves el texto
@@ -24,7 +24,7 @@ print(guid_list)
 i=0
 
 for i in range(len(guid_list)):
-    url_bucle = 'http://api.datosabiertos.enacom.gob.ar/api/v2/datastreams/'+ guid_list[i] + '/data.json/?auth_key=' + auth_key
+    url_bucle = 'http://api.datosabiertos.enacom.gob.ar/api/v2/datastreams/{guid}/data.json/?auth_key={clave}'.format(guid=guid_list[i],clave=auth_key)
     print()
     print(url_bucle)
     headers = {"User-Agent": "Mozilla/5.0"}
