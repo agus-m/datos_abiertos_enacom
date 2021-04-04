7ff5b1a2c5d404e5d913bbeddc9048bb145aaef7
agusm <57236016+agus-m@users.noreply.github.com>
2021-04-04 09:53:40 -0300

    Add files via upload

--- Request_Enacom_Json.py
+++ Request_Enacom_Json.py
@@ -1,12 +1,9 @@
 import requests
 import pandas  as pd
-import urllib.request
 import json
 
-
-# Busca la lista de datasets disponibles en resources.json, los carga en un dataframe y busca la columna guid,
-# para poder construir los get con esos id, después corre un loop con la petición de cada id
-# y guarda cada archivo en una carpeta local.
+# Busca la lista de datasets disponibles en resources.json, los carga en un dataframe, corre un loop con la petición get para cada guid
+# y guarda cada archivo .json en una carpeta local.
 
 auth_key = "A6xbR3TrMZW6ivfxJmAzo5QMGbxLLUYjLPatSzr4"
 url = 'http://api.datosabiertos.enacom.gob.ar/api/v2/resources.json/?auth_key={clave}'.format(clave=auth_key)
@@ -25,7 +22,6 @@ i=0
 
 for i in range(len(guid_list)):
     url_bucle = 'http://api.datosabiertos.enacom.gob.ar/api/v2/datastreams/{guid}/data.json/?auth_key={clave}'.format(guid=guid_list[i],clave=auth_key)
-    print()
     print(url_bucle)
     headers = {"User-Agent": "Mozilla/5.0"}
     respuesta_ds = requests.get(url_bucle, headers=headers).json()
@@ -33,3 +29,4 @@ for i in range(len(guid_list)):
     path = r'C:/Users/agus3/Desktop/Python/Enacom/Json/' + archivo
     with open(path, 'w') as f:
         json.dump(respuesta_ds, f)
+
